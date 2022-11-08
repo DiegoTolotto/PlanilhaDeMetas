b6e4c3d1f54b24e5dc8488671c1621c3e2591c9e
Diego Tolotto <diego.tolotto8@gmail>
2022-11-08 08:48:32 -0300

    Adcionando localStorege

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Looks } from './Looks';
 
 function App() {
+
+  const dataLooks = localStorage.getItem("looks")
+  
+
+  const [quantidadeLooks , setQuantidadeLooks] = useState(0);
+  const [entradas , setEntradas] = useState(0);
+  const [pagamento , setPagamento] = useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Looks />
   );
 }
 
 export default App;
+ 
---
+++ src/Looks/index.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import './styles.css';
+
+export const Looks = () => {
+    const [mes , setMes] = useState("");
+    const [dia , setDia] = useState("");
+    const [quantidade , setQuantidade] = useState(0);
+    const [entradas_Futuras , setEntradas_Futuras] = useState(0);
+    const [pagamento_Henry , setPagamento_Henry] = useState(0);
+
+    const handleSave = () => {
+        if(!mes || !dia || !quantidade || !entradas_Futuras || !pagamento_Henry) {
+            alert("Iforme todos os campos")
+            return;
+        } else if (mes < 1 || mes > 12) {
+            alert("Mês não encontrado")
+            return;
+        } else if (pagamento_Henry < 1) {
+            alert("Informe pagamento positivo")
+            return;
+        }
+
+
+    }
+
+
+    return (
+        <div>
+            <div>
+            <label>Mes</label>
+            <input value={mes} onChange={(e) => setMes(e.target.value)}></input>
+            </div>
+
+            <div>
+            <label>Dia</label>
+            <input value={dia} onChange={(e) => setDia(e.target.value)}></input>
+            </div>
+
+            <div>
+            <label>Quantidade</label>
+            <input value={quantidade} onChange={(e) => setQuantidade(e.target.value)}></input>
+            </div>
+
+            <div>
+            <label>Entradas_Futuras</label>
+            <input value={entradas_Futuras} onChange={(e) => setEntradas_Futuras(e.target.value)}></input>
+            </div>
+
+            <div>
+            <label>Pagamento_Henry</label>
+            <input value={pagamento_Henry} onChange={(e) => setPagamento_Henry(e.target.value)}></input>
+            </div>
+
+            <button onChange={handleSave}>Enviar</button>
+            </div>
+    )
+}
