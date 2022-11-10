5d6e9089ce3ceb7864e7e2167019e732c41193dc
Diego Tolotto <diego.tolotto8@gmail>
2022-11-10 13:04:15 -0300

    Adicioando saidas nas transações

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import { Looks } from './Looks';
 
 function App() {
 
-  const dataLooks = localStorage.getItem("looks");
+  const dataLooks = localStorage.getItem("transacoes");
   const [listaLook, setListaLook] = useState(dataLooks ? JSON.parse(dataLooks) : []);
 
-  const [quantidadeLooks , setQuantidadeLooks] = useState(0);
+  // const [quantidadeLooks , setQuantidadeLooks] = useState(0);
   const [entrada, setEntrada] = useState(0);
   const [pagamento , setPagamento] = useState(0);
+  console.log(entrada, pagamento)
 
   useEffect(() => {
-    const totalEntradas = listaLook.map((transacoes) => Number(transacoes.entradas))
+    const totalEntradas = listaLook.map((transacoes) => Number(transacoes.entrada));
+    const totalSaidas = listaLook.map((transacoes) => Number(transacoes.pagamentos));
+   
+    const entradaAcc = totalEntradas.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const saidaAcc = totalSaidas.reduce((acc, cur) => acc + cur, 0).toFixed(2);
 
-    const entradas = totalEntradas.reduce((acc, cur) => acc + cur, 0).toFixed(2);
-
-    setEntrada(`${entrada}`)
-  }, [listaLook]);
+    setEntrada(entradaAcc)
+    setPagamento(saidaAcc)
+  }, [listaLook]); 
 
   const handleAdd = (transacoes) => {
     const novoArrayTransacoes = [...listaLook, transacoes];
@@ -28,7 +32,7 @@ function App() {
   };
   
   return (
-    <Looks />
+    <Looks handleAdd={handleAdd}/>
   );
 }
 
--- src/Looks/index.js
+++ src/Looks/index.js
@@ -1,65 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './styles.css';
 
-export const Looks = () => {
-    const [mes , setMes] = useState("");
-    const [dia , setDia] = useState("");
-    const [quantidade , setQuantidade] = useState(0);
-    const [entradas , setEntradas] = useState(0);
-    const [pagamento_Henry , setPagamento_Henry] = useState(0);
+export const Looks = ({ handleAdd }) => {
+    const [entradas, setEntradas] = useState("");
+    const [pagamento, setPagamento] = useState(0);
 
 
     const generateID = () => Math.round(Math.random() * 1000);
 
     const handleSave = () => {
-        if(!mes || !dia || !quantidade || !entradas || !pagamento_Henry) {
-            alert("Iforme todos os campos")
-            return;
-        } else if (mes < 1 || mes > 12) {
-            alert("Mês não encontrado")
-            return;
-        } else if (pagamento_Henry < 1) {
-            alert("Informe pagamento positivo")
-            return;
-        }
-
+       
         const transacoes = {
             id: generateID(),
-            entradas: entradas,
-            saidas: pagamento_Henry,
-        } 
+            entrada: entradas,
+            pagamentos: pagamento,
+            }
 
+            
+
+        handleAdd(transacoes);
+
+        setEntradas("");
+        setPagamento("");
     }
 
 
     return (
         <div>
             <div>
-            <label>Mes</label>
-            <input value={mes} onChange={(e) => setMes(e.target.value)}></input>
+                <label>Entradas</label>
+                <input 
+                    value={entradas} 
+                    type="number" 
+                    onChange={(e) => setEntradas(e.target.value)}>
+                </input>
             </div>
 
             <div>
-            <label>Dia</label>
-            <input value={dia} onChange={(e) => setDia(e.target.value)}></input>
+                <label>Pagamento Henry</label>
+                <input 
+                    value={pagamento} 
+                    type="number" 
+                    onChange={(e) => setPagamento(e.target.value)}></input>
             </div>
 
-            <div>
-            <label>Quantidade</label>
-            <input value={quantidade} onChange={(e) => setQuantidade(e.target.value)}></input>
-            </div>
-
-            <div>
-            <label>Entradas_Futuras</label>
-            <input value={entradas_Futuras} onChange={(e) => setEntradas_Futuras(e.target.value)}></input>
-            </div>
-
-            <div>
-            <label>Pagamento_Henry</label>
-            <input value={pagamento_Henry} onChange={(e) => setPagamento_Henry(e.target.value)}></input>
-            </div>
-
-            <button onChange={handleSave}>Enviar</button>
-            </div>
+            <button onClick={handleSave}>Enviar</button>
+            
+        </div>
     )
 }
