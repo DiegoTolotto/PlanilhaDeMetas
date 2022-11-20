56435ddfa66d26100a75f910e035a104fc412858
Diego Tolotto <diego.tolotto8@gmail>
2022-11-20 15:27:37 -0300

    Estilizando cores e posições do resumo

--- src/Resume/index.js
+++ src/Resume/index.js
@@ -6,25 +6,29 @@ import "./resume.css"
 export const Resume = ({ entrada, saida, total, quantidade }) => {
     return (
         <div>
-            <div className="containerResume">
+            <div>
                 <div className="looks">
                     {/* 
                     Este é um campo que apresenta o numero total
                     de "looks" (Conjuntos, vestidos ou peças de 
                         roupa)                 
                     */}
+                    <p>Looks</p>
                     <Looks className="looks" value={quantidade} />
                     
                 </div>
 
-                <div>
+                <div className="containerResume">
                     <div className="entradas">
+                        <p>entradas</p>
                         <ResumeItem value={entrada} />
                     </div>
                     <div className="saidas">
+                        <p>saidas</p>
                         <ResumeItem value={saida} />
                     </div>
                     <div className="total">
+                        <p>total</p>
                         <ResumeItem value={total} />
                     </div>
                 </div>
